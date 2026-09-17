624b32df6d2b6803162a1e3905e483226ef49658
Jandui Junior <134757940+Janduijr@users.noreply.github.com>
2026-09-17 18:51:15 -0300

    aprimorando templates

--- PARTE 3 DJANGO/Projeto - Aula 1/projetos/views.py
+++ PARTE 3 DJANGO/Projeto - Aula 1/projetos/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import topic
 
 # Create your views here.
 
 def index(request):
     return render(request, 'projetos/index.html')
+
+def topics(request):
+    topics = topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request,'projetos/topic.html', context)
